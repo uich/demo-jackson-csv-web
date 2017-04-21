13904b2f0a743056ab06c32948fcf29a746e7853
uich <uich.ab.asklepios@gmail.com>
2017-04-21 18:32:51 +0900

    ダウンロード時のファイル名設定(IE用)

--- src/main/java/jp/uich/web/controller/advice/ResponseHeadersAdvice.java
+++ src/main/java/jp/uich/web/controller/advice/ResponseHeadersAdvice.java
@@ -1,6 +1,7 @@
 package jp.uich.web.controller.advice;
 
 import java.nio.charset.Charset;
+import java.nio.file.Paths;
 
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
@@ -30,9 +31,10 @@ public class ResponseHeadersAdvice implements ResponseBodyAdvice<Object> {
     Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request,
     ServerHttpResponse response) {
     if (((ServletServerHttpRequest) request).getServletRequest().getParameterMap().containsKey("download")) {
+      String fileName = Paths.get(request.getURI().getRawPath()).getFileName().toString();
       // For IE
       response.getHeaders().add("Content-Transfer-Encoding", "binary");
-      response.getHeaders().add("Content-Disposition", "attachment");
+      response.getHeaders().add("Content-Disposition", "attachment; filename=" + fileName);
 
       if (MappingJackson2CsvHttpMessageConverter.class.isAssignableFrom(selectedConverterType)) {
         response.getHeaders().setContentType(new MediaType(selectedContentType, Charset.forName("Shift_JIS")));
